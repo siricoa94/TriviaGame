9a5f4e24c8c471caf05c408cf590d7c10e678757
Andrew <siricoa94@github.com>
2019-07-05 17:09:13 -0400

    added timer to game, also added function for when timer runs out to display a message to user what the correct answer was, and to let them know they have ran out of time.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,63 @@
-$(document).ready(function () {
+$(document).ready(function () { 
+    var timerNumber = 30;
+    var intervalId;
+
     var questionObject = {
         questionOne: {
           georgeWashington:{
               georgeQuestion: "What river is George Washington known for crossing?",
               georgeWrongAnswerOne: "The Potomec",
-              georgeWrongAnserTwo: "River Thames",
+              georgeWrongAnswerTwo: "River Thames",
               georgeWrongAnswerThree: "The Nile",
               georgeRightAnswer: "The Delaware"
           }
         },
+        questionTwo: {
+            abrahamLincoln:{
+                abrahamQuestion: "What did Abraham Lincoln apparently grow up in?",
+                abrahamWrongAnswerOne: "A penthouse overlooking central park",
+                abrahamWrongAnswerTwo: "A cave",
+                abrahamWrongAnswerThree: "On a farm",
+                abrahamRightAnswer: "In a log cabin"
+            }
+        }
         // questionTwo:{
         //   type: "home",
         //   number: "212 555-1234"
         // }
     }
     $("#questionAsk").text("Start")
-    ($("#questionAsk").on("click" , function () {
-        $("#questionAsk").text("testquestion")
-    }))
+    $("#questionAsk").on("click" , function () {
+        //timer() {wait this many seconds then change the page}
+        $("#questionAsk").text(questionObject.questionOne.georgeWashington.georgeQuestion)
+        $("#questionAnswerSlotOne").text(questionObject.questionOne.georgeWashington.georgeWrongAnswerOne)
+        $("#questionAnswerSlotOne").on("click" , function () {
+            //timer.stop()
+        })
+        $("#questionAnswerSlotTwo").text(questionObject.questionOne.georgeWashington.georgeWrongAnswerThree)
+        $("#questionAnswerSlotThree").text(questionObject.questionOne.georgeWashington.georgeRightAnswer)
+        $("#questionAnswerSlotFour").text(questionObject.questionOne.georgeWashington.georgeWrongAnswerTwo)
+        //timer() {wait this many seconds then change the page}
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+          }
+      
+          function decrement() {
+      
+            timerNumber--;
+      
+            $("#timeLapse").html("Time Remaining: " + timerNumber);
+      
+            if (timerNumber === 0) {
+                stop();
+                $("#questionAsk").text("Sorry, you ran out of time, the correct answer was "+questionObject.questionOne.georgeWashington.georgeRightAnswer);
+                
+              
+            }
+          }
+          run();
+    })
+    // if(){}
+    
 })
